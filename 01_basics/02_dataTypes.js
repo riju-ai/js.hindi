d17d0084dd564e742e460b24e6e30c7340f2474e
Swarnava Dhabal <65087434+riju-ai@users.noreply.github.com>
2024-02-12 15:53:43 +0000

    02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";  // treated as a newer version of javascript
+
+// alart ( 3+3)  // we are usuing nodejs, not browser thats why it shows error in console
+
+console.log (3
+    +
+    3)  // code readability should be high
+
+console.log("swarnava")
+
+let name = "swarnava"
+let age = 20
+let isLoggdin = false
+let state = null
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/ false
+// null => standalone value (empty)
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null)  // output-> object
+console.log(typeof undefined);   // output-> undefined
+
