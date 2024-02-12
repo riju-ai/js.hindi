52540c2b1192adc5670e61ae8e12ed29a8748b51
Swarnava Dhabal <65087434+riju-ai@users.noreply.github.com>
2024-02-12 20:41:44 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -63,7 +63,7 @@ let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => ture
@@ -76,5 +76,55 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//  ************ Operations  ************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// etc
+
+
+let str1 = "hello"
+let str2 = " swarnava"
+let str3 = str1 + str2 
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2+"2");
+// console.log("1"+2+2);
+
+
+
+// console.log(+true); 
+
+// +true is possible and value is 1 bt   
+//true+ is not considerable 
+
+
+
+// console.log(+"")   
+
+// not so much useable or not preferable
+
+
+
+
+let num1, num2, num3
+
+num1= num2= num3= 2+2
+
+
+let gameCounter = 100
+gameCounter++;                  //prefix and postfix from jxand ndm
+console.log(gameCounter);
