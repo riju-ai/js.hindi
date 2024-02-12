05ca57b078d9cee753fedab5369f42897da6e63c
Swarnava Dhabal <65087434+riju-ai@users.noreply.github.com>
2024-02-12 16:31:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,80 @@
+/* let score = 33
+
+console.log (typeof score);
+console.log (typeof (score));   */   // in this case the output came as number
+
+
+
+
+// let score = "33abc"
+
+// console.log (typeof score);
+// console.log (typeof (score));     // in this case output came as string
+
+
+// let valueInNumber = Number(score)
+// console.log (typeof valueInNumber);
+// console.log (valueInNumber);
+
+
+
+
+// let score = null
+
+// console.log (typeof score);
+// console.log (typeof (score));     // in this case output came as string
+
+
+// let valueInNumber = Number(score)
+// console.log (typeof valueInNumber);
+// console.log (valueInNumber);
+
+
+
+
+// let score = true
+
+// console.log (typeof score);
+// console.log (typeof (score));     // in this case output came as string
+
+
+// let valueInNumber = Number(score)
+// console.log (typeof valueInNumber);
+// console.log (valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// "true" => 1
+// "false" => 0
+
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+// 1 => ture
+// "" => false
+// "swarnava" => true
+
+
+
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
