931b983524438b237512d82582cb0d642859bb54
Swarnava Dhabal <65087434+riju-ai@users.noreply.github.com>
2024-02-13 17:32:53 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,56 @@
+// // Primitive
+
+// 7 types of Primitive datatypes are
+
+// String, Number, Boolean, Null, Undefined, Symbol, Bigint
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null       // null is null not 0 value
+let userEmail;                 // (undefined)
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log( id === anotherId);
+
+// const bigNumber = 365627562838659283n
+
+
+// Reference Non-Primitive datatypes are 
+
+// Array, Object, Functions
+
+const heros = ["shaktiman", "spiderman", "batman"]
+
+let myObj= {
+    name: "Swarnava",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myFunction);
+
+// console.log( typeof bigNumber );      // type of is used to know the datatype of the variable
+
+// console.log( typeof outsideTemp);
+
+
+
+Primitive datatypes:
+String - string
+Number - number
+Boolean - Boolean
+Null - object(****)
+Undefined - undefined
+BigInt - bigint
+
+Non-primitive datatypes:
+Array - object
+Function - function(object)
+Object - object
