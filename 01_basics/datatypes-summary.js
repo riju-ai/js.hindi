ff093c71760c04e94a4969629dc241623b2a9814
Swarnava Dhabal <65087434+riju-ai@users.noreply.github.com>
2024-02-17 07:39:15 +0000

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,56 +1,78 @@
-// // Primitive
+// // // Primitive
 
-// 7 types of Primitive datatypes are
+// // 7 types of Primitive datatypes are
 
-// String, Number, Boolean, Null, Undefined, Symbol, Bigint
+// // String, Number, Boolean, Null, Undefined, Symbol, Bigint
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null       // null is null not 0 value
-let userEmail;                 // (undefined)
+// const isLoggedIn = false
+// const outsideTemp = null       // null is null not 0 value
+// let userEmail;                 // (undefined)
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log( id === anotherId);
+// console.log( id === anotherId);
 
-// const bigNumber = 365627562838659283n
+// // const bigNumber = 365627562838659283n
 
 
-// Reference Non-Primitive datatypes are 
+// // Reference Non-Primitive datatypes are 
 
-// Array, Object, Functions
+// // Array, Object, Functions
 
-const heros = ["shaktiman", "spiderman", "batman"]
+// const heros = ["shaktiman", "spiderman", "batman"]
 
-let myObj= {
-    name: "Swarnava",
-    age: 20,
+// let myObj= {
+//     name: "Swarnava",
+//     age: 20,
+// }
+
+// const myFunction = function(){
+//     console.log("hello world");
+// }
+
+// console.log(typeof myFunction);
+
+// // console.log( typeof bigNumber );      // type of is used to know the datatype of the variable
+
+// // console.log( typeof outsideTemp);
+
+
+
+// Primitive datatypes:
+// String - string
+// Number - number
+// Boolean - Boolean
+// Null - object(****)
+// Undefined - undefined
+// BigInt - bigint
+
+// Non-primitive datatypes:
+// Array - object
+// Function - function(object)
+// Object - object
+
+let ironMan = "tony"
+
+let avanger = ironMan
+avanger = "marvel"
+
+console.log(ironMan);
+console.log(avanger);
+
+let userOne =
+{
+    email : "user@google.com",
+    upi : "user@ybl"
+    
 }
 
-const myFunction = function(){
-    console.log("hello world");
-}
+let userTwo = userOne
 
-console.log(typeof myFunction);
+userTwo.email = "Swarnava@google.com"
 
-// console.log( typeof bigNumber );      // type of is used to know the datatype of the variable
-
-// console.log( typeof outsideTemp);
-
-
-
-Primitive datatypes:
-String - string
-Number - number
-Boolean - Boolean
-Null - object(****)
-Undefined - undefined
-BigInt - bigint
-
-Non-primitive datatypes:
-Array - object
-Function - function(object)
-Object - object
+console.log(userOne.email);
+console.log(userTwo.email);
