476854c6049189030bc68af2bf7d776bd75928a7
Swarnava Dhabal <65087434+riju-ai@users.noreply.github.com>
2024-03-04 19:21:25 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,10 +13,25 @@
 
 function addTwoNumbers (number1, number2){
    
-    let result = number1 + number2
-    return result
+    // let result = number1 + number2
+    // console.log("swarnava");         // this will be printed bt after the result this will not printed
+    // return result                   // after this result nothing is printed but if you write console before 
+                                      // result then that will be printed 
+    
+    return number1 + number2                                                 
 }
 
 
 const result = addTwoNumbers(3,4)                  //(.....)------- arguments
-console.log("result", );
+
+// console.log("result:", result );
+
+function loginUserMssage (username = "sam") {
+    if(username === undefined){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMssage("swarnava"));
+console.log(loginUserMssage("riju"));
