be7d1b6d8b55dbc7a8132dc14e445d7f80be4737
Swarnava Dhabal <65087434+riju-ai@users.noreply.github.com>
2024-03-04 16:30:54 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+
+// function sayMyName(){
+//     console.log("r");
+//     console.log("i");
+//     console.log("j");
+//     console.log("u");
+//     }
+    // sayMyName()
+
+// function addTwoNumbers (number1, number2){         //(......)------ parameters
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers (number1, number2){
+   
+    let result = number1 + number2
+    return result
+}
+
+
+const result = addTwoNumbers(3,4)                  //(.....)------- arguments
+console.log("result", );
