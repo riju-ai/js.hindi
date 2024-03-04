cb70997121e0ba0e95bd06d076c99b2bbfbd714e
Swarnava Dhabal <65087434+riju-ai@users.noreply.github.com>
2024-03-04 12:50:09 +0000

    object

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,55 @@
+// const tinderUser = new object ()  -------- singletonobject
+const tinderUser = {}    // ------------- nonsingleton object
+
+tinderUser.id = "123ab"
+tinderUser.name = "Pratap"
+tinderUser.isLoggedIn = false
+
+//  console.log(tinderUser);
+
+const regularser = {
+    email: "someone@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Swarnava",
+            lastname: "Dhabal"
+        }
+
+    }
+}
+// console.log(regularser.fullname.userfullname.firstname);
+
+const obj1 = { 1:"A" , 2:"B"}
+const obj2 = { 3:"C" , 4:"D"}
+
+
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+
+const obj3 = {...obj1,...obj2}   //   -------- Spread Operator
+console.log(obj3);
+
+// when the value came form databse
+
+const user = [
+    {
+        id: 1,
+        email: "swarnava@gmail.com"
+    },
+    {
+        id: 1,
+        email: "swarnava@gmail.com"
+    },
+    {
+        id: 1,
+        email: "swarnava@gmail.com"
+    }
+]
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
